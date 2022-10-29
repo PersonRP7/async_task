75db9b5586de8bebe0ed667026b82b4519fc8109
Person <zemlja-trp@yandex.com>
2022-10-29 19:26:17 +0200

    Implemented star_wars and pork Stream class

---
+++ streams.py
@@ -0,0 +1,40 @@
+import requests
+# https://swapi.dev/api/people
+
+class Stream:
+
+    star_wars_list = []
+    pork_list = []
+
+    def __init__(self, star_wars_url:str, pork_url:str) -> None:
+        self.store_star_wars(star_wars_url)
+        self.pork_list(pork_url)
+
+    def store_star_wars(self, url:str) -> None:
+        """
+        https://swapi.dev/api/people 
+
+        requires flattening.
+        """
+        data = requests.get(url)
+        data_json = data.json()
+        for i in data_json['results']:
+            self.star_wars_list.append(i['name'])
+
+    def store_pork(self, url:str)->None:
+        data = url
+        data_json = data.json()
+        for i in data_json:
+            self.pork_list.append(url)
+
+    def access(self):
+        print(self.star_wars_list)
+    
+        
+
+
+
+    
+
+    
+
