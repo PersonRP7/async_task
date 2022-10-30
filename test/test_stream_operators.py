175816e11ef01818dea0bc0a0f875fc02e0fa34c
Person <zemlja-trp@yandex.com>
2022-10-30 20:01:46 +0100

    Implemented Test Class for Stream Operators. Implemented setUp class and test_vowel_comparison_returns_none

---
+++ test/test_stream_operators.py
@@ -0,0 +1,28 @@
+import sys
+sys.path += ['./']
+#Allows the tests to be run from the root directory, as python doesn't treat
+#the current working directory as a package.
+
+import unittest
+import requests
+from streams import Stream
+from stream_operators import StreamOperators
+
+class TestStreamOperators(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        base_stream = Stream("https://swapi.dev/api/people", "https://baconipsum.com/api/?type=meat-and-filler")
+        star_wars = base_stream.list_getter("star_wars_list")
+        pork = base_stream.list_getter("pork_list")
+        cls.star_wars = star_wars
+        cls.pork = pork
+
+    def test_vowel_comparison_returns_none(self):
+        response = StreamOperators.vowel_comparison(sw = self.star_wars, p = self.pork)
+        self.assertEqual(response, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
