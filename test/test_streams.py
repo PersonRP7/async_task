3ee68beb3d5f68e2af6cb6596d09cd4f94c60a12
Person <zemlja-trp@yandex.com>
2022-10-30 06:25:45 +0100

    Added sys.path += ['./'] to to test_streams

--- test/test_streams.py
+++ test/test_streams.py
@@ -1,5 +1,12 @@
+import sys
+sys.path += ['./']
+#Allows the tests to be run from the root directory, as python doesn't treat
+#the current working directory as a package.
+
 import unittest
 import requests
+from streams import Stream
+
 
 #py -m unittest
 
@@ -8,6 +15,14 @@ class TestStreams(unittest.TestCase):
     def test_star_wars_reachable(self):
         response = requests.get("https://swapi.dev/api/people")
         self.assertEqual(response.status_code, 200)
+
+    def test_pork_reachable(self):
+        response = requests.get("https://baconipsum.com/api/?type=meat-and-filler")
+        self.assertEqual(response.status_code, 200)
+
+    def test_star_wars_list_populated(self):
+        stream = Stream("https://swapi.dev/api/people", "https://baconipsum.com/api/?type=meat-and-filler")
+        self.assertGreater(len(stream.star_wars_list), 0)
 
 if __name__ == '__main__':
     unittest.main()
