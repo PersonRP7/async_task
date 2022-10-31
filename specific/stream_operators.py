f182ecf967a199588b530b73d5406c33c660c407
Person <zemlja-trp@yandex.com>
2022-10-31 05:54:04 +0100

    specific/stream_operators implemented skewer first version.

--- specific/stream_operators.py
+++ specific/stream_operators.py
@@ -1,8 +1,13 @@
 import operator
+import random
 from streams import Stream
 
 class StreamOperators:
 
+    vowels = [
+        "a", "e", "i", "o", "u",
+        "A", "E", "I", "O", "U"
+    ]
    
     @staticmethod
     def length_comparison(lorem:str, pirate:str)->dict:
@@ -12,6 +17,27 @@ class StreamOperators:
         }
         print(f"{max(length_data.items(), key=operator.itemgetter(1))[0]} has the most items.")
         return length_data
+
+    # @staticmethod
+    # def skewer(lorem:str, pirate:str)->dict:
+    #     lorem_skewer = ""
+    #     pirate_skewer = ""
+    #     for i in range(1):
+    #         print('-'.join(random.sample(lorem, random.randint(1,len(lorem)))))
+
+    @staticmethod
+    def skewer(lorem:str, pirate:str)->dict:
+        lorem_skewer = ""
+        pirate_skewer = ""
+        for i in range(1):
+            lorem_skewer += '-'.join(random.sample(lorem, random.randint(1,len(lorem))))
+        print(lorem_skewer)
+
+
+StreamOperators.skewer(
+    Stream.stream_to_list(Stream.lorem),
+    Stream.stream_to_list(Stream.pirate)
+)
 
 # print(
 #     StreamOperators.length_comparison(
