c22aae6b5235fa5ceffd79ec52a3fce32682b692
Person <zemlja-trp@yandex.com>
2022-11-01 02:23:09 +0100

    test_streams : moved class instatiation to setUp method. Refactored tests to use setUp stream instance.

--- version1/test/test_streams.py
+++ version1/test/test_streams.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path += ['./']
 #Allows the tests to be run from the root directory, as python doesn't treat
 #the current working directory as a package.
@@ -11,6 +12,14 @@ from streams import Stream
 #py -m unittest
 
 class TestStreams(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        base_stream = Stream("https://swapi.dev/api/people", "https://baconipsum.com/api/?type=meat-and-filler")
+        star_wars = base_stream.list_getter("star_wars_list")
+        pork = base_stream.list_getter("pork_list")
+        cls.star_wars = star_wars
+        cls.pork = pork
     
     def test_star_wars_reachable(self):
         response = requests.get("https://swapi.dev/api/people")
@@ -21,12 +30,14 @@ class TestStreams(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_star_wars_list_populated(self):
-        stream = Stream("https://swapi.dev/api/people", "https://baconipsum.com/api/?type=meat-and-filler")
-        self.assertGreater(len(stream.star_wars_list), 0)
+        
+        self.assertGreater(len(self.star_wars), 0)
     
     def test_pork_list_populated(self):
-        stream = Stream("https://swapi.dev/api/people", "https://baconipsum.com/api/?type=meat-and-filler")
-        self.assertGreater(len(stream.pork_list), 0)
 
+        self.assertGreater(len(self.pork), 0)
+
+    
+        
 if __name__ == '__main__':
     unittest.main()
