89b33e82d8472f2e97180252f738bf6cc282f0f4
Person <zemlja-trp@yandex.com>
2022-11-01 07:21:12 +0100

    Commented out the base_stream call in stream_operators

--- version1/stream_operators.py
+++ version1/stream_operators.py
@@ -7,13 +7,13 @@ import random
 from streams import Stream
 
 
-base_stream = Stream("https://swapi.dev/api/people", 
-"https://baconipsum.com/api/?type=meat-and-filler",
- "https://pokeapi.co/api/v2/pokemon"
-)
-star_wars = base_stream.list_getter("star_wars_list")
-pork = base_stream.list_getter("pork_list")
-pokemon = base_stream.list_getter("pokemon_list")
+# base_stream = Stream("https://swapi.dev/api/people", 
+# "https://baconipsum.com/api/?type=meat-and-filler",
+#  "https://pokeapi.co/api/v2/pokemon"
+# )
+# star_wars = base_stream.list_getter("star_wars_list")
+# pork = base_stream.list_getter("pork_list")
+# pokemon = base_stream.list_getter("pokemon_list")
 
 class StreamOperators:
 
