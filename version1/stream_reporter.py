8b8e4906d3acbdafa8047b53f82af065d1a534f9
Person <zemlja-trp@yandex.com>
2022-11-01 07:00:37 +0100

    Added base_stream and pokemon var to stream_reporter

--- version1/stream_reporter.py
+++ version1/stream_reporter.py
@@ -1,9 +1,13 @@
 from streams import Stream
 from stream_operators import StreamOperators
 
-base_stream = Stream("https://swapi.dev/api/people", "https://baconipsum.com/api/?type=meat-and-filler")
+base_stream = Stream("https://swapi.dev/api/people", 
+"https://baconipsum.com/api/?type=meat-and-filler",
+ "https://pokeapi.co/api/v2/pokemon"
+)
 star_wars = base_stream.list_getter("star_wars_list")
 pork = base_stream.list_getter("pork_list")
+pokemon = base_stream.list_getter("pokemon_list")
 
 def reporter()->None:
     print("")
